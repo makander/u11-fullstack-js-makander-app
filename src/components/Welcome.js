8945ef9a6cd936c423497e8c229295011a02c4bf
Nils Makander <makander@gmail.com>
2019-09-26 14:00:55 +0200

    rebuilt welcome page

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,38 +1,59 @@
-import React from 'react';
-import {
-  Header,
-  Grid,
-  Button,
-  Icon,
-  Image,
-  Item,
-  Label,
-} from 'semantic-ui-react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect, useContext } from 'react';
+import { Header, Grid, Item, Label, ItemHeader } from 'semantic-ui-react';
+import { AuthContext } from '../context/AuthContext';
+import axios from 'axios';
 
 const Welcome = () => {
+  const { authStatus } = useContext(AuthContext);
+  const [coffeePots, setCoffeePots] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:5000/api/coffeepots/')
+      .then((data) => {
+        setCoffeePots(data.data.result, () => setLoading(false));
+        console.log(data.data.result[0]);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <Grid container centered columns={1}>
       <Grid.Row>
         <Header size="huge">Welcome to CoffePot</Header>
       </Grid.Row>
-      <Item.Group divided>
-        <Item>
-          <Item.Image src="https://react.semantic-ui.com/images/wireframe/image.png" />
 
-          <Item.Content>
-            <Item.Header as="a">CoffeePot #1</Item.Header>
-            <Item.Meta>Finns i köket</Item.Meta>
-            <Item.Description>Det är 2 koppar kaffe kvar</Item.Description>
+      {!loading ? (
+        <div>Loading</div>
+      ) : (
+        <Item.Group divided>
+          {console.log(coffeePots)}
+          {coffeePots.map((item) => (
+            <Item>
+              <Item.Image src="https://react.semantic-ui.com/images/wireframe/image.png" />
 
-            <Item.Extra>
-              <Label>Delete</Label>
-              <Label>Update</Label>
-            </Item.Extra>
-          </Item.Content>
-        </Item>
-      </Item.Group>
+              <Item.Content>
+                <ItemHeader>{item.name}</ItemHeader>
+                <br />
+                {item.status.map((item) => (
+                  <Item.Description as="a">
+                    {item.weight}
+                    {item.time}
+                  </Item.Description>
+                ))}
+                <Item.Meta>{item.description}</Item.Meta>
+                <Item.Extra>
+                  <Label>Delete</Label>
+                  <Label>Update</Label>
+                </Item.Extra>
+              </Item.Content>
+            </Item>
+          ))}
+        </Item.Group>
+      )}
     </Grid>
   );
 };
+
 export default Welcome;
